016da6d45892286250a861fd71982717b9fcd3fa
umarshad36 <umarshad36@gmail.com>
2023-07-17 01:55:16 +0100

    vpc
    vpc

---
+++ main.tf
@@ -0,0 +1,13 @@
+locals {
+  name = "auto-winner"
+}
+# provider "vault" {
+#   token   = "s.KGqGspjsv4lE4M4YTTVc2KXj"
+#   address = "https://thinkeod.com"
+# }
+module "vpc" {
+  source                 = "./module/vpc"
+  vpc_cidr        
+         = "10.0.0.0/16"
+  tag-vpc                = "${local.name}-vpc"
+}
